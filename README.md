25331e00150fcc729f2d1dbe01613a6fa1a20b2d
Guilherme Vieira de Souza <v.guilherme@academico.ifrn.edu.br>
2024-07-18 14:26:14 -0300

    new class Modif

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ classDiagram
     Teste : +Int idade
     Teste : -Int cpf
     Teste : #Int conta
+    Teste : getName()
+    Teste : setName
+    Teste : getCPF()
 ```
--- Teste.py
+++ Teste.py
@@ -4,3 +4,15 @@ class Teste():
         self.idade = 14
         self.__cpf = 101001010101
         self._conta = 1111
+
+
+class Modif(Teste):
+    teste = Teste()
+    def setName(self):
+        self.nome = input('digite nome: ')
+        
+    def getName(self):
+        return print(self.nome)
+    
+    def getCPF(self):
+        return print(self._Teste__cpf)
